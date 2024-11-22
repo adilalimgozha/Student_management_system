a900ccead186b1613fe9c7639ef80a6fc4e5b19d
adilalimgozha <alimgozha2003@gmail.com>
2024-11-23 01:34:33 +0500

    caching finished

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Course, Enrollment
+from students.models import Student
 from django.contrib.auth import get_user_model
 from students.serializers import StudentsSerializer
 
@@ -12,9 +13,29 @@ class CoursesSerializer(serializers.ModelSerializer):
 
     
 class EnrollmentSerializer(serializers.ModelSerializer):
-    course = CoursesSerializer(many=True)  # Вложенные курсы
-    student = StudentsSerializer(many=True)  # Вложенные студенты
+    # Вложенные сериализаторы для отображения данных
+    student = StudentsSerializer(read_only=True, many=True)
+    course = CoursesSerializer(read_only=True, many=True)
+
+    # Поля для записи ID студентов и курсов
+    student_id = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all(), write_only=True, many=True)
+    course_id = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all(), write_only=True, many=True)
 
     class Meta:
         model = Enrollment
-        fields = ('student', 'course')
+        fields = ('student', 'course', 'student_id', 'course_id')
+
+    def create(self, validated_data):
+        # Извлекаем ID студентов и курсов из данных
+        student_ids = validated_data.pop('student_id')
+        course_ids = validated_data.pop('course_id')
+
+        # Создаем объект Enrollment
+        enrollment = Enrollment.objects.create(**validated_data)
+
+        # Устанавливаем связи для студентов и курсов
+        enrollment.student.set(student_ids)  # Множественное присваивание через set
+        enrollment.course.set(course_ids)    # Множественное присваивание через set
+
+        return enrollment
+
--- courses/views.py
+++ courses/views.py
@@ -12,6 +12,7 @@ from rest_framework.exceptions import NotFound
 from .models import Course, Enrollment
 import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django.core.cache import cache
 
 class CoursesAPIListPagination(PageNumberPagination):
     page_size = 2
@@ -49,9 +50,8 @@ class CoursesAPIView(APIView):
     filterset_class = CoursesFilter 
 
     def get(self, request):
-        courses = Course.objects.all()
 
-        serializer = CoursesSerializer(courses, many=True)
+        courses = Course.objects.all()
 
         filterset = self.filterset_class(request.GET, queryset=courses)
         if filterset.is_valid():
@@ -64,7 +64,7 @@ class CoursesAPIView(APIView):
         if paginated_students is not None:
             return paginator.get_paginated_response(CoursesSerializer(paginated_students, many=True).data)
         
-        return Response({'courses': serializer.data}, status=status.HTTP_200_OK)
+        return Response({'students': CoursesSerializer(courses, many=True).data}, status=status.HTTP_200_OK)
     
 
 class CoursesAPIViewEdit(APIView):
@@ -99,6 +99,17 @@ class CoursesAPIViewEnroll(APIView):
 
 
     def get(self, request):
+
+        student_id = request.GET.get('student', None)
+        instructor_id = request.GET.get('instructor', None)
+
+        cache_key = f"courses_list_student_{student_id}_instructor_{instructor_id}"
+        
+        cached_courses = cache.get(cache_key)
+        if cached_courses:
+            return Response({"cached courses enrollment": cached_courses}, status=status.HTTP_200_OK)
+        
+
         enrollments = Enrollment.objects.all()
 
         serializer = EnrollmentSerializer(enrollments, many=True)
@@ -112,13 +123,26 @@ class CoursesAPIViewEnroll(APIView):
         paginator = EnrollmentsAPIListPagination()
         paginated_students = paginator.paginate_queryset(enrollments, request)
         if paginated_students is not None:
-            return paginator.get_paginated_response(EnrollmentSerializer(paginated_students, many=True).data)
-
-        return Response({'enrollments': serializer.data}, status=status.HTTP_200_OK)
+            serialized_data = EnrollmentSerializer(paginated_students, many=True).data
+            response_data = paginator.get_paginated_response(serialized_data).data
+            
+            cache.set(cache_key, response_data, timeout=600)  # Cache for 10 minutes
+            return Response({"courses enrollment": response_data}, status=status.HTTP_200_OK)
+        
+        response_data = {'courses enrollment': serializer.data}
+        cache.set(cache_key, response_data, timeout=600)  # Cache for 10 minutes
+        
+        return Response(response_data, status=status.HTTP_200_OK)
 
     def post(self, request):
+        student_id = request.GET.get('student', None)
+        instructor_id = request.GET.get('instructor', None)
         serializer = EnrollmentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+
+            cache_key = f"courses_list_student_{student_id}_instructor_{instructor_id}"
+            cache.delete(cache_key)
+
             return Response({"msg": "Student enrolled to the course successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
